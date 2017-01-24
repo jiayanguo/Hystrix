ddf7a0ad61b9dc633572f02ec74cfb0f3816b496
jguo <guojiayanc@gmail.com>
2017-01-23 23:29:21 -0700

    add system out

--- hystrix-dashboard/src/main/java/com/netflix/hystrix/dashboard/stream/Application.java
+++ hystrix-dashboard/src/main/java/com/netflix/hystrix/dashboard/stream/Application.java
@@ -10,5 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        System.out.println("===================server started at:====================");
+        System.out.println("http://localhost:8080");
     }
 }
